e7faf26f5f928357582228ba7a89efaddf489db8
Fikir <fikirtesfaye8@gmail.com>
2026-07-16 17:21:10 +0300

    data base configuration

--- DjangoBase/settings.py
+++ DjangoBase/settings.py
@@ -83,12 +83,27 @@ WSGI_APPLICATION = 'DjangoBase.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import os
+import dj_database_url
+# Fallback to local sqlite if DB_HOST isn't set, otherwise use RDS postgres/mysql
+if os.environ.get('DB_HOST'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql', # Change to mysql if choosing MySQL RDS
+            'NAME': os.environ.get('DB_NAME', 'appdb'),
+            'USER': os.environ.get('DB_USER', 'admin'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'HOST': os.environ.get('DB_HOST'),
+            'PORT': os.environ.get('DB_PORT', '5432'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'LOCATION': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
